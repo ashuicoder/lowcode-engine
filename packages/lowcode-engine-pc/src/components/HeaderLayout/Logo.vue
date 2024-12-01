acba2707711a28a254b9427a2eb779d5c418934e
ashuicoder <1249735120@qq.com>
2024-12-01 21:33:19 +0800

    ✨ feat: logo渐变色

---
+++ packages/lowcode-engine-pc/src/components/HeaderLayout/Logo.vue
@@ -0,0 +1,9 @@
+<template>
+  <n-gradient-text type="info" size="18px"> 低代码平台 </n-gradient-text>
+</template>
+
+<script setup lang="ts">
+import { NGradientText } from 'naive-ui'
+</script>
+
+<style scoped></style>
