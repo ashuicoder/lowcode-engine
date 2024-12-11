bc1bd94621d2d8e54e6f38d6a99d8c2aa4842658
ashuicoder <1249735120@qq.com>
2024-12-11 15:41:58 +0800

    ✨ feat: 大纲功能

--- packages/lowcode-engine-pc/src/components/Outline/inex.vue
+++ packages/lowcode-engine-pc/src/components/Outline/inex.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="rg-mr-4 rg-bg-white rg-w-[200px] rg-overflow-auto rg-custom-scrollbar rg-py-4">
-    <NTree :data="componentTree" label-field="name" key-field="id"></NTree>
+    <NTree :data="componentTree" label-field="name" key-field="id" :node-props="nodeProps"></NTree>
   </div>
 </template>
 
 <script setup lang="ts">
-import { NTree } from 'naive-ui'
-import { componentTree } from '@packages/data'
+import { NTree, type TreeOption } from 'naive-ui'
+import { componentTree, currentNode } from '@packages/data'
+import type { IComponentNode } from '@packages/types'
+
+function nodeProps({ option }: { option: TreeOption }) {
+  return {
+    onClick() {
+      if (option.id === currentNode.value?.id) return
+      currentNode.value = option as unknown as IComponentNode
+    },
+  }
+}
 </script>
 
 <style scoped></style>
